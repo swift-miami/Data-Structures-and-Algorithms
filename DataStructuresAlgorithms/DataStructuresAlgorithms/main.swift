579899a1a00d4d912b5614e685beca14a1d35f2c
Iván <me@ivancr.com>
2019-04-06 14:48:25 -0400

    Create Xcode project

---
+++ DataStructuresAlgorithms/DataStructuresAlgorithms/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DataStructuresAlgorithms
+//
+//  Created by Ivan Corchado Ruiz on 4/6/19.
+//  Copyright © 2019 ivancr. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
